f1d6a77b526d41d7f196451ab503eb44e149c7d3
Adam Sawicki <adam.sawicki172@gmail.com>
2017-12-16 00:23:59 +0100

    Basic Login with in-memory authentication

---
+++ src/main/java/com/crmax/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package com.crmax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping(value = "/dashboard")
+    public String showDashboard(){
+        return "home";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello-page</title>
+</head>
+<body>
+
+    <h1>Hello CRMAX</h1>
+
+</body>
+</html>
